949738e334738bd2444ef32ec5dc5f2470e29756
marcomora03 <marco.morandinrettori@gmail.com>
2025-08-25 19:56:47 +0200

    Update volterra.cpp

--- volterra.cpp
+++ volterra.cpp
@@ -2,44 +2,42 @@
 #include <cmath>
 #include <vector>
 namespace lv {
-  size_t Simulation::size() const
-{
-  return ev_vector_.size();
-}
 Constants initialize_const() {
   double a, b, c, d;
-  do {
+  std::cout<<"inserire parametri: a(fertlità prede) b(mortalità prede) c(fertlità predatori) d(mortalità predatori)"<<std::endl;
     std::cin >> a >> b >> c >> d;
     if (a < 0 || b < 0 || c < 0 || d < 0) {
-      std::cout<<"Parametri errati"<<std::endl;}
-    }
-  while (a < 0 || b < 0 || c < 0 || d < 0);
+     
+     throw std::runtime_error("Parametri non validi");}
   return Constants{a, b, c, d};
 }
 Point initialize_point() {
   double x0, y0;
-  do {
+    std::cout<<"inserire valori inziali di densità di popolazione delle prede (x) e dei predatori (y)"<<std::endl;
     std::cin >> x0 >> y0;
     if (x0 < 0 || y0<0) {
-      std::cout<<"Inserire punti maggiori o uguali a zero"<<std::endl;}
-    }
-   while (x0 < 0 || y0 < 0);
+      throw std::runtime_error("Punti non validi");}
+    
 
   return Point{x0, y0};
 }
-Point Simulation::calc_point(double x_prev, double y_prev,double H, double dt) {
-  double x = x_prev + parameters.a * (1 - y_prev) * x_prev * dt;
-  double y = y_prev + parameters.d * (x_prev - 1) * y_prev * dt;
-  H = -parameters.d*std::log(x)+parameters.c*x+parameters.b*y-parameters.a*std::log(y);
-  return Point(x, y, H);
+double Simulation::calculate_H(double x, double y){
+  return -parameters.d*std::log(x)+parameters.c*x+parameters.b*y-parameters.a*std::log(y);
+}
+Point Simulation::calc_next_point(const Point& prev_point,  double dt) {
+  double x = prev_point.x + parameters.a * (1 - prev_point.y) *  prev_point.x * dt;
+  double y = prev_point.y + parameters.d * ( prev_point.x - 1) * prev_point.y * dt;
+  double H = calculate_H(x* (parameters.d / parameters.c),y* (parameters.a / parameters.b));
+  return Point{x, y, H};
 }
 Simulation::Simulation(const Point& p_i, const Constants& c) : parameters(c) {
+  
   ev_vector_.push_back(give_relative_point(p_i));
 };
 void Simulation::e_evolve(double dt, int nsteps) {
   for (int i = 0; i < nsteps; i++) {
     const Point& last = ev_vector_.back();
-    Point next{calc_point(last.x, last.y,last.H, dt)};
+    Point next{calc_next_point(last, dt)};
     ev_vector_.push_back(next);
   }
 }
@@ -48,12 +46,13 @@ void Simulation::e_evolve(double dt, int nsteps) {
     double y_rel = p.y * (parameters.b / parameters.a);
     return Point{x_rel, y_rel};
   }
-   const Point& Simulation::a_p_get_step_result(int n) {
-    static Point
-        res;  // static fa persistere un oggetto oltre il suo normale scope
-    res.x = ev_vector_[n].x * (parameters.d / parameters.c);
-    res.y = ev_vector_[n].y * (parameters.a / parameters.b);
-      res.H =-parameters.d*std::log(ev_vector_[n].x)+parameters.c*ev_vector_[n].x+parameters.b*ev_vector_[n].y-parameters.a*std::log(ev_vector_[n].y);
-    return res;
-  }
-}  // namespace lv
+    Point Simulation::a_p_get_step_result(int n) {  //a_p_ a inizio nome è un riferimento ai comandi che faranno uso di questo metodo, cosi come e_ in e_evolve
+    if (n < 0 || static_cast<size_t>(n) >= ev_vector_.size()) {
+throw std::runtime_error("non esiste un step alla posizione richiesta");}
+    double x_abs = ev_vector_[n].x * (parameters.d / parameters.c);
+    double y_abs = ev_vector_[n].y * (parameters.a / parameters.b);
+    double H = calculate_H(x_abs,y_abs);
+    return Point{x_abs,y_abs,H}; 
+  
+}  
+}
