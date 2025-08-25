fb722e2187c883c7c2b0426a4c1b8a3ab7fa37c6
marcomora03 <marco.morandinrettori@gmail.com>
2025-08-25 13:05:39 +0200

    Update volterra.hpp

--- volterra.hpp
+++ volterra.hpp
@@ -8,8 +8,10 @@ namespace lv {
 struct Point {
   double x;
   double y;
+  double H;
   Point() = default;                             // costruttore di default
-  Point(double x_, double y_) : x(x_), y(y_) {}  // costruttore parametrizzato
+//  Point(double x_, double y_) : x(x_), y(y_){};
+  Point(double x_, double y_, double H_=0.0) : x(x_), y(y_), H(H_){}// costruttore parametrizzato
 };
 struct Constants {
   const double a, b, c, d;
@@ -23,29 +25,19 @@ class Simulation {
  private:
   Constants parameters;
   std::vector<Point> ev_vector_;
-  Point give_relative_point(const Point& p) {
-    double x_rel = p.x * (parameters.c / parameters.d);
-    double y_rel = p.y * (parameters.b / parameters.a);
-    return Point{x_rel, y_rel};
-  }
-  Point calc_point(double x_prev, double y_prev, double dt);
+  Point give_relative_point(const Point& p);
+  Point calc_point(double x_prev, double y_prev,double H, double dt);
 
  public:
   Simulation(const Point& p_i, const Constants& c);
+  size_t size() const;
 
   // costruttore che inserisce in evvector il p iniziale inseriti da utente
   // inizializza i parametri
   const std::vector<Point>& evolution_vector() const { return ev_vector_; }
   void e_evolve(double dt, int nsteps);
 
-  const Point& a_p_get_step_result(int n) {
-    static Point
-        res;  // static fa persistere un oggetto oltre il suo normale scope
-    res.x = ev_vector_[n].x * (parameters.d / parameters.c);
-    res.y = ev_vector_[n].y * (parameters.a / parameters.b);
-    return res;
-  }
-
+  const Point& a_p_get_step_result(int n);
 };
 }  // namespace lv
 #endif
