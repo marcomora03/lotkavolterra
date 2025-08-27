5c7a8284943c7a8f3b4059e3a12d0e13260fdecc
marcomora03 <marco.morandinrettori@gmail.com>
2025-08-27 03:19:30 +0200

    Update volterra.hpp

--- volterra.hpp
+++ volterra.hpp
@@ -1,42 +1,39 @@
-#ifndef LV_REGRESSION_HPP  // è una macro puo avere qualsiasi nome ma si usa la
-                           // convenzione qua usata (nome file maiusc)
+#ifndef LV_REGRESSION_HPP
 #define LV_REGRESSION_HPP
 #include <iostream>
-#include <stdexcept>
 #include <vector>
 namespace lv {
 struct Point {
   double x;
   double y;
-  double H;                             // costruttore di default
-//  Point(double x_, double y_) : x(x_), y(y_){};
-  Point(double x_, double y_, double H_=0.0) : x(x_), y(y_), H(H_){}// costruttore parametrizzato
+  double H;
+  Point() : x(0.0), y(0.0), H(0.0) {}
+  Point(double x_, double y_, double H_ = 0.0) : x(x_), y(y_), H(H_) {}
 };
 struct Constants {
-  const double a, b, c, d;
+  double a, b, c, d;
+  Constants() : a(0), b(0), c(0), d(0) {}
   Constants(double a_, double b_, double c_, double d_)
       : a(a_), b(b_), c(c_), d(d_) {}
 };
-Constants initialize_const();
-Point initialize_point();
-
+void initialize_const(Constants& k);
+void set_zeropoint(Point& p0);
+void abs_to_rel(Point& p, const Constants& c);
+void rel_to_abs(Point& p, const Constants& c);
 class Simulation {
  private:
   Constants parameters;
   std::vector<Point> ev_vector_;
-  Point give_relative_point(const Point& p);
-  Point calc_next_point(const Point& prev_point, double dt);
-  double calculate_H(double x, double y);
+  Point calc_next_point(const Point& prev_point, double dt) const;
+  double calculate_H(double x, double y) const;
 
  public:
   Simulation(const Point& p_i, const Constants& c);
 
-  // costruttore che inserisce in evvector il p iniziale inseriti da utente
-  // inizializza i parametri
   const std::vector<Point>& evolution_vector() const { return ev_vector_; }
-  void e_evolve(double dt, int nsteps);
+  void evolve(double dt, int nsteps);
 
-  Point a_p_get_step_result(int n);
+  Point get_step_result(int n) const;
 };
-}  // namespace lv
+}  
 #endif
